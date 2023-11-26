401ebbc42b12b01aee92177a456080e7309f5da0
Dio <vinilimasampaio@gmail.com>
2023-11-26 12:55:51 -0300

    Ajeitei a formatação do código

--- headers_files/table.h
+++ headers_files/table.h
@@ -6,11 +6,11 @@
 #define PROJETO_FINAL_ITP_TABLE_H
 #include "database.h"
 
-bool name_table_is_valid(const char *table_name);                          // Verifica se o nome da tabela é válido
-void create_table(const char *filename);                                  // Cria uma nova tabela
-void list_tables(const char *filename);                                  // Lista as tabelas do banco de dados
-void delete_table(const char *filename);        // Deleta uma tabela do banco de dados
+bool name_table_is_valid(const char *table_name);          // Verifica se o nome da tabela é válido
+void create_table(const char *filename);                   // Cria uma nova tabela
+void list_tables(const char *filename);                    // Lista as tabelas do banco de dados
+void delete_table(const char *filename);                   // Deleta uma tabela do banco de dados
 
 #endif //PROJETO_FINAL_ITP_TABLE_H
 
-// última atualização: 2023-11-25 -- dio
+// última atualização: 2023-11-26 -- dio
