f17f04221a619b3bf86328faf8a7fa18a6bd2575
Dio <vinilimasampaio@gmail.com>
2023-11-26 12:10:06 -0300

    Terminei as funções que a gente estabeleceu pro table
    Documentei algumas coisas no create_table e implementei list_tables & delete_tables, na delete eu coloquei um algoritmo de busca pra comparar os nomes da tabela com a entrada do usuário.

--- headers_files/table.h
+++ headers_files/table.h
@@ -6,10 +6,10 @@
 #define PROJETO_FINAL_ITP_TABLE_H
 #include "database.h"
 
-bool name_table_is_valid(const char *table_name);   // Verifica se o nome da tabela é válido
-void create_table(const char *filename);           // Cria uma nova tabela
-void list_tables(const char *filename);           // Lista as tabelas do banco de dados
-void delete_table();                             // Deleta uma tabela do banco de dados
+bool name_table_is_valid(const char *table_name);                          // Verifica se o nome da tabela é válido
+void create_table(const char *filename);                                  // Cria uma nova tabela
+void list_tables(const char *filename);                                  // Lista as tabelas do banco de dados
+void delete_table(const char *filename);        // Deleta uma tabela do banco de dados
 
 #endif //PROJETO_FINAL_ITP_TABLE_H
 
