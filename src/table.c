c89c4db1bd6fe526ffc753add431c1fce3b8d836
Dio <vinilimasampaio@gmail.com>
2023-12-15 23:05:02 -0300

    Update table.c
    Deixei o listTableData mais bunito

--- src/table.c
+++ src/table.c
@@ -66,37 +66,99 @@ void listTableData(char* tableName) {
 
     Table* table = &tables[tableIndex];
 
+    // Calculate maximum width for each column
+    int columnWidths[MAX_COLUMNS];
     for (int i = 0; i < table->numColumns; i++) {
-        printf("%-15s", table->columns[i].name);
+        columnWidths[i] = strlen(table->columns[i].name);
+        for (int j = 0; j < table->numRows; j++) {
+            int cellWidth;
+            Cell cell = table->rows[j].cells[i];
+            switch (cell.type) {
+                case INT:
+                    cellWidth = snprintf(NULL, 0, "%d", cell.value.intValue);
+                    break;
+                case FLOAT:
+                    cellWidth = snprintf(NULL, 0, "%.2f", cell.value.floatValue);
+                    break;
+                case DOUBLE:
+                    cellWidth = snprintf(NULL, 0, "%.2lf", cell.value.doubleValue);
+                    break;
+                case CHAR:
+                    cellWidth = snprintf(NULL, 0, "%c", cell.value.charValue);
+                    break;
+                case STRING:
+                    cellWidth = strlen(cell.value.stringValue);
+                    break;
+                case EMPTY:
+                    cellWidth = 1;
+                    break;
+            }
+            if (cellWidth > columnWidths[i]) {
+                columnWidths[i] = cellWidth;
+            }
+        }
+        columnWidths[i] += 2; // Add extra spaces for readability
+    }
+
+    // Calculate total width
+    int totalWidth = 0;
+    for (int i = 0; i < table->numColumns; i++) {
+        totalWidth += columnWidths[i] + 3; // for data, "| ", and " "
+    }
+    totalWidth += 1; // for the last "|"
+
+    // Print line above column headers
+    for (int i = 0; i < totalWidth; i++) {
+        printf("-");
     }
     printf("\n");
 
+    // Print column headers
+    for (int i = 0; i < table->numColumns; i++) {
+        printf("| %-*s ", columnWidths[i], table->columns[i].name);
+    }
+    printf("|\n");
+
+    // Print line below column headers
+    for (int i = 0; i < totalWidth; i++) {
+        printf("-");
+    }
+    printf("\n");
+
+    // Print each row
     for (int i = 0; i < table->numRows; i++) {
         for (int j = 0; j < table->numColumns; j++) {
             Cell cell = table->rows[i].cells[j];
+            printf("| ");
             switch (cell.type) {
                 case INT:
-                    printf("%-15d", cell.value.intValue);
+                    printf("%-*d ", columnWidths[j], cell.value.intValue);
                     break;
                 case FLOAT:
-                    printf("%-15.2f", cell.value.floatValue);
+                    printf("%-*.*f ", columnWidths[j], 2, cell.value.floatValue);
                     break;
                 case DOUBLE:
-                    printf("%-15.2lf", cell.value.doubleValue);
+                    printf("%-*.*lf ", columnWidths[j], 2, cell.value.doubleValue);
                     break;
                 case CHAR:
-                    printf("%-15c", cell.value.charValue);
+                    printf("%-*c ", columnWidths[j], cell.value.charValue);
                     break;
                 case STRING:
-                    printf("%-15s", cell.value.stringValue);
+                    printf("%-*s ", columnWidths[j], cell.value.stringValue);
                     break;
                 case EMPTY:
-                    printf("%-15s", " ");
+                    printf("%-*s ", columnWidths[j], " ");
                     break;
             }
         }
-        printf("\n");
+        printf("|\n");
     }
+
+    // Print line at the end of the table
+    for (int i = 0; i < totalWidth; i++) {
+        printf("-");
+    }
+    printf("\n");
 }
 
 /**
